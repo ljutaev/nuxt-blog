103f9006827542b94cccb7901cbc3d54345f6e5e
ljutaev <ljutaev@gmail.com>
2019-05-06 08:38:12 +0300

    Post Component Styling

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,19 +1,53 @@
 <template>
-    <article class="post">
-
-    </article>
+    <el-card
+        shadow="always"
+        :body-style="{padding: 0}"
+        class="post"
+    >
+        <header slot="header" class="post__header">
+            <h3>Post title</h3>
+            <small><i class="el-icon-time"></i> {{ new Date().toLocaleString() }}</small>
+        </header>
+        <div class="post__body">
+            <img src="https://www.mcny.org/sites/default/files/New-York-14-10-2489.jpg" alt="Post Image" class="post__image">
+        </div>
+        <footer class="post__footer">
+            <el-button round @click="openPost">Открыть</el-button>
+            <span>
+                <i class="el-icon-message"></i>
+                12
+            </span>
+        </footer>
+    </el-card>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        openPost() {
+            console.log('Open Post')
+            alert(1)
+        }
+    }
 }
 </script>
 
 <style lang="sass" scoped>
     .post
-        height: 200px
-        border: 1px solid black
-        margin-bottom: 2rem
+        margin-bottom: 1.5rem
+        &__header
+            display: flex
+            justify-content: space-between
+            align-items: center
+        &__body
+
+        &__image
+            width: 100%   
+            height: auto
+        &__footer
+            padding: 1rem
+            display: flex
+            justify-content: space-between
+            align-items: center
 </style>
 
