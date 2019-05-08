a325b9f7be2c42b7698b3be7b9f3604db003804e
ljutaev <ljutaev@gmail.com>
2019-05-08 14:56:56 +0300

    Posts list

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -4,6 +4,22 @@
     </div>
 </template>
 
+<script>
+export default {
+    computed: {
+        error() {
+            return this.$store.getters.error
+        }
+    },
+    watch: {
+        error(value) {
+            console.log(value)
+            this.$message.error(value)
+        }
+    }
+}
+</script>
+
 <style lang="sass" scoped>
     .empty-layout
         display: flex
--- pages/admin/list.vue
+++ pages/admin/list.vue
@@ -1,10 +1,52 @@
 <template>
-    <div>List</div>
+  <el-table
+    :data="posts"
+    style="width: 100%">
+    <el-table-column 
+        prop="title"
+        label="Название"
+    />
+    <el-table-column label="Дата">
+      <template slot-scope="{row: {date}}">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ new Date(date).toLocaleString() }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="Name"
+      width="180">
+      <template slot-scope="scope">
+        <el-popover trigger="hover" placement="top">
+          <p>Name: {{ scope.row.name }}</p>
+          <p>Addr: {{ scope.row.address }}</p>
+          <div slot="reference" class="name-wrapper">
+            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+          </div>
+        </el-popover>
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="Operations">
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
     layout: 'admin',
-    middleware: ['admin-auth']
+    middleware: ['admin-auth'],
+    async asyncData({store}) {
+        const posts = await store.dispatch('post/fetchAdmin')
+        return {posts}
+    }
 }
 </script>
