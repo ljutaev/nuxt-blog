8d8e8b2a4312fe90eebcb7ea0bf45d87a769b285
ljutaev <ljutaev@gmail.com>
2019-05-17 09:01:56 +0300

    Post Controller

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,25 +1,56 @@
 const Post = require('../models/post.model')
 
-module.exports.create = async (req, res) = {
+module.exports.create = async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: `/${req.file.filename}`
+    })
+
+    try {
+        await post.save()
+        res.status(201).json(post)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
+
+module.exports.getAll = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({date: -1})
+        res.json(posts)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
+
+module.exports.getById = async (req, res) => {
+    try {
+        await Post.findById(req.params.id).populate('comments').exec((error, post) => {
+            res.json(post)
+        })
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
+
+module.exports.update = async (req, res) => {
+    const $set = {
+        text = req.body.text
+    }
+    try {
+        const post = await Post.findOneAndUpdate({
+            _id: req.params.id
+        }, {$set}, {new: true})
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
+
+module.exports.remove = async (req, res) => {
 
 }
 
-module.exports.getAll = async (req, res) = {
-
-}
-
-module.exports.getById = async (req, res) = {
-
-}
-
-module.exports.update = async (req, res) = {
-
-}
-
-module.exports.remove = async (req, res) = {
-
-}
-
-module.exports.addView = async (req, res) = {
+module.exports.addView = async (req, res) => {
 
 }
